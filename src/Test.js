0817c3b751c84aecb834c897f34745ff394fb825
ming <515335161@qq.com>
2021-01-14 23:01:04 +0800

    init

---
+++ src/Test.js
@@ -0,0 +1,10 @@
+
+function Test() {
+  return (
+    <div className="App">
+      Test组件
+    </div>
+  );
+}
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Test from './Test'
 import reportWebVitals from './reportWebVitals';
 
+console.log(App())
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
+    <Test/>
   </React.StrictMode>,
   document.getElementById('root')
 );
