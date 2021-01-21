0238ec09997801bcb2cc0aaf6cdde096de100b1e
ming <515335161@qq.com>
2021-01-21 23:10:48 +0800

    sync

--- src/components/FunctionCom.jsx
+++ src/components/FunctionCom.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 function FunctionCom() {
   console.log(this);
-  let test = () => {
-    console.log(this);
+  let test = (e) => {
+    console.log('e',e);
+    console.log('this',this);
   };
   let inFn = function () {
     console.log("试一下");
   };
   return (
-    <div
-      onClick={(e) => {
-        console.log("onClick-this", this);
-        test();
-        inFn();
-        console.log(e);
-      }}
-    >
-      函数式组件
+    <div>
+      <div
+        onClick={(e) => {
+          console.log("onClick-this", this);
+          test();
+          inFn(e);
+          console.log(e);
+        }}
+      >
+        函数式组件
+      </div>
+      <button onClick={test}>惦记我 看控制台</button>
     </div>
   );
 }
